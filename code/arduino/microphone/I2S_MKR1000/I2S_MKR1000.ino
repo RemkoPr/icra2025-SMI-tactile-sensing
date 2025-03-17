258e1c37d010935adab7c8155bd32ff59b9565be
rproesma <remko.proesmans@ugent.be>
2025-03-17 16:12:20 +0100

    updated MKR1000 mic readout code

--- code/arduino/microphone/I2S_MKR1000/I2S_MKR1000.ino
+++ code/arduino/microphone/I2S_MKR1000/I2S_MKR1000.ino
@@ -8,16 +8,20 @@
  * Arduino/Genuino Zero, MKR family and Nano 33 IoT
  * ICS43432:
    * GND connected GND
-   * 3.3V connected to 3.3V (Zero, Nano) or VCC (MKR)
-   * WS connected to pin 0 (Zero) or 3 (MKR) or A2 (Nano)
-   * CLK connected to pin 1 (Zero) or 2 (MKR) or A3 (Nano)
-   * SD connected to pin 9 (Zero) or A6 (MKR) or 4 (Nano)
+   * 3.3V connected to VCC
+   * WS connected to pin D3
+   * SCK connected to pin D2
+   * SD connected to pin A6
 
  created 17 November 2016
- by Sandeep Mistry adapted by Remko Proesmans
+ by Sandeep Mistry 
+ adapted 2024
+ by Remko Proesmans
  */
 
 #include <I2S.h>
+
+#define DEBUG false
 
 #define FS  20000 
 #define SAMPLE_BUFFER_LEN   50
@@ -27,7 +31,6 @@ int start_time_ = micros();
 int time_passed = 0;
 uint8_t sample_buffer[SAMPLE_BUFFER_LEN * SAMPLE_BYTE_LEN] = {0};
 int sample_ctr = 0;
-uint8_t start_seq[10] = {1};
 
 void setup() {
   Serial.begin(115200);
@@ -44,34 +47,40 @@ void setup() {
 
 void loop() {
   int32_t sample = I2S.read();
-  
-  /*time_passed = micros() - start_time_;
+
+#if DEBUG
+  time_passed = micros() - start_time_;
   if (time_passed > 1000000) {
+    Serial.print("Readout frequency: ");
     Serial.println(sample_ctr/(time_passed/1000000.0));
     start_time_ = micros();
     sample_ctr = 0;
-  }*/
+  }
+#endif
+
   if (sample) {
-    //Serial.println("x: " + String(sample) + " -> " + String(sample >> 8));
+#if DEBUG
+    Serial.println("x: " + String(sample) + " -> " + String(sample >> 8));
+#endif
     sample_buffer[sample_ctr*SAMPLE_BYTE_LEN] = sample >> 24;
     sample_buffer[sample_ctr*SAMPLE_BYTE_LEN + 1] = (sample >> 16) & 0xFF;
     sample_buffer[sample_ctr*SAMPLE_BYTE_LEN + 2] = (sample >> 8) & 0xFF;
-    /*Serial.println(String(sample_buffer[sample_ctr]) + " " 
-                    + String(sample_buffer[sample_ctr + 1]) + " " 
-                    + String(sample_buffer[sample_ctr + 2]));*/
 
     sample_ctr++;
   }
   if(sample_ctr == SAMPLE_BUFFER_LEN) {
     sample_ctr = 0;
-    Serial.write(sample_buffer, SAMPLE_BUFFER_LEN*SAMPLE_BYTE_LEN);
 
-    /*Serial.println("------------------------------------------");
+#if DEBUG
+    Serial.println("------------------------------------------");
     for(int i=0 ; i < SAMPLE_BUFFER_LEN ; i++) {
       Serial.println(String(sample_buffer[i*SAMPLE_BYTE_LEN]) + " " 
                     + String(sample_buffer[i*SAMPLE_BYTE_LEN + 1]) + " " 
                     + String(sample_buffer[i*SAMPLE_BYTE_LEN + 2]));
     }
-    Serial.println("------------------------------------------");*/
+    Serial.println("------------------------------------------");
+#else
+    Serial.write(sample_buffer, SAMPLE_BUFFER_LEN*SAMPLE_BYTE_LEN);
+#endif
   }
 }
